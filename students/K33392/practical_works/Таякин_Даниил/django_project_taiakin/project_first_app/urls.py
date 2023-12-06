9692fc8deb27d437df5367a5c8c23c2e39a978a8
Daniel Taiakin <daniel@pewpee.com>
2023-12-07 01:22:20 +0300

    feat: practical work 2.2

--- students/K33392/practical_works/Таякин_Даниил/django_project_taiakin/project_first_app/urls.py
+++ students/K33392/practical_works/Таякин_Даниил/django_project_taiakin/project_first_app/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('owner/<int:owner_id>/', views.owner_detail),
+    path('owners/', views.owner_list),
+    path('car/<int:pk>/', views.CarRetrieveView.as_view()),
+    path('cars/', views.CarList.as_view())
 ]
--- students/K33392/practical_works/Таякин_Даниил/django_project_taiakin/project_first_app/views.py
+++ students/K33392/practical_works/Таякин_Даниил/django_project_taiakin/project_first_app/views.py
@@ -1,11 +1,26 @@
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.shortcuts import render
-from django.http import Http404 
-from .models import CarOwner
+from django.http import HttpRequest, HttpResponse, Http404 
+from .models import CarOwner, Car
 
-def owner_detail(request, owner_id):
+def owner_detail(request: HttpRequest, owner_id: int) -> HttpResponse:
     try:
         owner = CarOwner.objects.get(pk=owner_id)
     except CarOwner.DoesNotExist:
         raise Http404("Owner does not exist")
     return render(request, 'owner.html', {'owner': owner})
 
+
+def owner_list(request: HttpRequest) -> HttpResponse:
+    return render(request, 'owner_list.html', {'owners': CarOwner.objects.all()})
+
+
+class CarList(ListView):
+    model = Car
+    template_name = 'car_list.html'
+
+
+class CarRetrieveView(DetailView):
+    model = Car
+    template_name = 'car.html'
